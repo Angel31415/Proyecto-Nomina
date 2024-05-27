b5f670d5aef934c994cd75ad1a6db06acd54b6cf
Emmanuel <131670585+Angel31415@users.noreply.github.com>
2024-05-27 09:01:28 -0500

    inicio sesion
    modificacion de redireccionamiento de los archivos

--- front/src/components/layaouts/pages/inicioSesion/InicioSesion.jsx
+++ front/src/components/layaouts/pages/inicioSesion/InicioSesion.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "./Iniciosesion.css"
+import "./inicioSesion/Iniciosesion.css"
 import { Link, useNavigate } from 'react-router-dom';
 
 export function Iniciosesion() {
